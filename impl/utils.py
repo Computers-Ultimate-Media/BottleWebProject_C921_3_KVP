ff14278bd69b296238e8525650eeced57ad55cb7
qwonix <roman.qwonix@gmail.com>
2022-06-02 17:14:40 +0300

    matrix correction check update

--- impl/utils.py
+++ impl/utils.py
@@ -6,4 +6,12 @@ def convert_list_to_square_matrix(arr:list, size:int) -> list:
     return A
 
 def isCorrectMatrix(matrix:list) -> bool:
+    if type(matrix) is not list:
+        return False
+
+    if len(matrix) == 0:
+        return False
+    if type(matrix[0]) is not list:
+        return False
+
     return all (len (row) == len (matrix) for row in matrix)
