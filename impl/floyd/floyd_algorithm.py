f455c5db46e0ffac7e2ea29ecb0ba8c34d0ceb53
Gauliux <artyomprokopenko.ru@gmail.com>
2022-06-06 16:55:42 +0300

    add operable floyd algorithm

---
+++ impl/floyd/floyd_algorithm.py
@@ -0,0 +1,34 @@
+
+from typing import List
+
+class Floyd:
+	graph:		List[List[int]]
+	shortest: 	List[List[int]]
+	paths: 		List[List[int]]
+	size:		int
+	
+	def __FindWays(self):
+		for i in range(self.size):
+			for j in range(self.size):
+				if(self.graph[i][j] == float('inf')):
+					self.paths[i][j] = float('inf')
+				else:
+					self.paths[i][j] = j
+					
+	def __FloydShortestPaths(self):
+		for k in range(self.size):
+			for i in range(self.size):
+				for j in range(self.size):
+					if ((k != i) and (self.shortest[i][k] != float('inf')) and (k != j) and (self.shortest[k][j] != float('inf')) and (self.shortest[i][j] == float('inf') or (self.shortest[i][j] > self.shortest[i][k] + self.shortest[k][j]))):
+						self.paths[i][j] = self.paths[i][k]
+						self.shortest[i][j] = self.shortest[i][k] + self.shortest[k][j]
+						
+	
+	def __init__(self, graph: List[List[int]]):
+		self.size = len(graph)
+		self.graph = graph
+		self.shortest = graph
+		self.paths = [[0 for _ in range(self.size)] for _ in range(self.size)]
+	
+		self.__FindWays()
+		self.__FloydShortestPaths()
